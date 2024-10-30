935aaba1f0308e805dab11394e865ff744cc935a
Kunli Zhang <kunlizhang7@gmail.com>
2024-10-30 16:55:50 -0400

    Use custom colors

--- wander-app/src/components/CategoryButton.jsx
+++ wander-app/src/components/CategoryButton.jsx
@@ -10,23 +10,19 @@ function CategoryButton({ imageSrc, text }) {
   return (
     <button
       onClick={handleClick}
-      className={`relative w-64 h-40 rounded-lg overflow-hidden transition-all duration-200 ${
-        isClicked ? 'shadow-[5px_5px_15px_rgba(255,140,0,1)]' : 'shadow-none'
+      className={`relative w-52 h-40 rounded-xl overflow-hidden transition-all duration-200 ${
+        isClicked ? 'shadow-orange-category' : 'shadow-none'
       }`}
-      style={{
-        // Custom inline shadow to make it more intense and offset
-        boxShadow: isClicked ? '5px 5px 1px rgba(255, 140, 0, 1)' : 'none',
-      }}
-    >
+      >
       {/* Background Image */}
       <img src={imageSrc} alt="button background" className="absolute inset-0 w-full h-full object-cover" />
       
       {/* Black Tint Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="absolute inset-0 bg-neutralBlack bg-opacity-50"></div>
 
       {/* Text Overlay */}
       <div className="absolute inset-0 flex items-center justify-center">
-        <span className="text-white font-semibold text-xl">{text}</span>
+        <span className="text-neutralWhite font-semibold text-xl">{text}</span>
       </div>
     </button>
   );
--- wander-app/tailwind.config.js
+++ wander-app/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryGreen: '#325D59',
+        primaryGreen2: '#43AA8B',
+        secondaryTeal: '#788FCE',
+        secondaryOrange: '#FA9161',
+        neutralBlack: '#0C0C20',
+        neutralWhite: 'FAFFFD'
+      },
+      boxShadow: {
+        'orange-category': '5px 5px 1px rgba(250,145,97,1)',
+      },
+    },
   },
   plugins: [],
 }
