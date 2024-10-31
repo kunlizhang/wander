0664cb1ba54c19109b694949bc20a64afd6d810b
Kunli Zhang <kunlizhang7@gmail.com>
2024-10-31 15:13:38 -0400

    Blur when press enter

--- wander-app/src/components/ClearableInput.jsx
+++ wander-app/src/components/ClearableInput.jsx
@@ -1,28 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ClearIcon from '@mui/icons-material/Clear';
 
 function ClearableInput({ placeholder }) {
-  const [inputValue, setInputValue] = useState('');
+    const inputRef = useRef(null);
 
-  const handleClear = () => setInputValue(''); // Clear the input value
+    const [inputValue, setInputValue] = useState('');
 
-  return (
-    <div className="relative">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-        placeholder={placeholder}
-        className="w-full p-2.5 text-gray-800 placeholder-gray-400 bg-gray-100 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-primaryGreen2"
-      />
-    <button
-        onClick={handleClear}
-        className="absolute inset-y-0 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
-    >
-        <ClearIcon />
-    </button>
-    </div>
-  );
+    const handleClear = () => setInputValue(''); // Clear the input value
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            event.target.blur();
+        };
+    };
+
+    return (
+        <div className="relative">
+        <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder={placeholder}
+            className="w-full p-2.5 text-gray-800 placeholder-gray-400 bg-gray-100 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-primaryGreen2"
+        />
+        <button
+            onClick={handleClear}
+            className="absolute inset-y-0 right-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+        >
+            <ClearIcon />
+        </button>
+        </div>
+    );
 }
 
 export default ClearableInput;
