96f0a8dc12608666a492e70b69f3da55a21ad2c0
Kunli Zhang <kunlizhang7@gmail.com>
2024-11-01 12:03:23 -0400

    Background fade

--- wander-app/src/pages/GeneratedItinerariesPage.jsx
+++ wander-app/src/pages/GeneratedItinerariesPage.jsx
@@ -3,11 +3,15 @@ import React from "react";
 function GeneratedItinerariesPage() {
     return (
         <div className="flex justify-center w-full h-full ">
-            <div className="w-96 h-screen bg-cover bg-center bg-[url('assets/downtownTripBackground.png')]">
-                <div className="text-4xl font-bold mb-12">Hi Jimmy!</div>
-                <div className="text-2xl font-bold mt-6">What do you enjoy doing?</div>
-                <div className="mt-4"></div>
-                <div className="mt-4"></div>
+            <div className="relative w-96 h-screen bg-cover bg-center bg-[url('assets/downtownTripBackground.png')]">
+                <div className="absolute inset-0 bg-gradient-to-b from-neutralWhite to-transparent"></div>
+                <div className="relative">
+                    <div className="mt-20"></div>
+                    <div className="text-4xl font-bold mb-12">Seattle</div>
+                    <div className="text-2xl font-bold mt-6">What do you enjoy doing?</div>
+                    <div className="mt-4"></div>
+                    <div className="mt-4"></div>
+                </div>
             </div>
         </div>
     );
