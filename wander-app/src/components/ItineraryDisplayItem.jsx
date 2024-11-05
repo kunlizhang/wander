2c2fbb5b1747a54c754868404efe306ec75096bd
Kunli Zhang <kunlizhang7@gmail.com>
2024-11-05 15:01:34 -0500

    Add profile picture support

--- wander-app/src/components/ItineraryDisplayItem.jsx
+++ wander-app/src/components/ItineraryDisplayItem.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import OverlappingProfilePictures from "./OverlappingProfilePictures";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import fanPics from "../backend/fan-pics.json";
 
 function ItineraryDisplayItem({ title, durationText, image, fans=[], nextHandler, previousHandler}) {
     const fanCount = fans.length;
 
     const fanText = fanCount < 3 ? "" : fans[0] + ", " + fans[1] + " and " + (fanCount - 2) + " others love this itinerary!"; 
+
+    const displayedFans = fans.slice(0, 3).map((fan) => fanPics[fan] || "assets/defaultFanPic.jpg");
+
     return (
         <div className={`relative w-80 h-64 rounded-2xl overflow-hidden`}>
             <img src={image} alt="button background" className="absolute inset-0 w-full h-full object-cover" />
@@ -15,7 +19,7 @@ function ItineraryDisplayItem({ title, durationText, image, fans=[], nextHandler
                 <div className="ml-4 mb-4 text-neutralWhite font-bold text-lg">{title}</div>
                 <div className="absolute bottom-2 ml-4 mb-4 text-neutralWhite text-xs">
                     <div className="flex">
-                        <OverlappingProfilePictures images={["assets/bars.jpg", "assets/brunch.jpg", "assets/parks.jpg"]} />
+                        <OverlappingProfilePictures images={displayedFans} />
                         <div className="ml-2 p-2">{fanText}</div>
                     </div>
                 </div>
