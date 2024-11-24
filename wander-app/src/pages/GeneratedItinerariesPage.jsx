1b36539ae6494ab256cbe7b2e4aad7a7a085bd17
Sherry Xue <sherryxue@lev-05144.apn.wlan.private.upenn.edu>
2024-11-24 16:47:34 -0500

    made edit button more prominent in v2 for experiment design

--- wander-app/src/pages/GeneratedItinerariesPage.jsx
+++ wander-app/src/pages/GeneratedItinerariesPage.jsx
@@ -43,6 +43,8 @@ function GeneratedItinerariesPage() {
                                     d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
                                 />
                             </svg>
+                            <span className="text-small font-medium">Edit</span>
+
                         </button>
                         <ItineraryDisplayCarousel items={seattleItinerary} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex}/>
                     </div>
